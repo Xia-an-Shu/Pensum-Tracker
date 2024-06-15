726e30138e9404b679cc31e6569788ce9994d0d4
XiaYun <88670910+Xia-an-Shu@users.noreply.github.com>
2024-06-15 18:01:32 -0500

    Temp removal of Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import { AppRouter } from "./components/routes/AppRouter";
 
 function App() {
   return (
-      <Auth0Provider
-          domain="dev-t4k4tx5owk02vb5k.us.auth0.com"
-          clientId="8ZuZCFv24cLeuiM3JHHzJWkoom17KGqb"
-          redirectUri={window.location.origin + "/Pensum-Tracker/home"}
-      >
+    //   <Auth0Provider
+    //       domain="dev-t4k4tx5owk02vb5k.us.auth0.com"
+    //       clientId="8ZuZCFv24cLeuiM3JHHzJWkoom17KGqb"
+    //       redirectUri={window.location.origin + "/Pensum-Tracker/home"}
+    //   >
           <AppRouter />
 
-      </Auth0Provider>
+    //   </Auth0Provider>
   );
 }
 
